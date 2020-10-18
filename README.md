69ebbbdd5aa221f1a772ace4f971750d8066d2d0
Brian Parker <btparker70@gmail.com>
2020-10-17 22:26:57 -0700

    final touches. checking to see if webpage will work properly

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ Thanks for visiting! My name is Brian and I am a web development student through
 # Password Generator
 Password generating webpage. BCS week 3 homework
 
-![screenshot](https://github.com/btparker70/Password-Generator/blob/main/assets/images/readmeimage.png?raw=true)
+![screenshot](https://github.com/btparker70/Password-Generator/blob/main/assets/images/readMeImage.png?raw=true)
 
 # Table of Contents
 
@@ -30,13 +30,13 @@ https://btparker70.github.io/Password-Generator/
 
 # What I learned
 
-This project was my first big step into implementing javascript with HTML and CSS. This page uses both buttons and forms, which requied some understanding of how javascript queries the various elements on the html document and assigns them values. After working with the basics of both html and javascript on their own, watching them work together here was exciting.
+This project was my first big step into implementing javascript with HTML and CSS. This page uses both buttons and forms, which requires some understanding of how javascript queries the various elements on the html document and assigns them values. After working with the basics of both html and javascript on their own, watching them work together here was exciting.
 
-As far as the code goes, I tried to reduce as much as I could into functions since there is a lot of redundancy with the types of characters. Also, this project helped get me better-aquainted with boolean values and their proper shorthand usage in places like while loops. 
+As far as the code goes, I tried to reduce as much as I could into functions since there is a lot of redundancy with the types of characters. The code is more complicated than it needs to be, but I felt like setting up the code so that it's easily improved upon later was best. Also, this project helped get me better-acquainted with boolean values and their proper shorthand usage in places like while loops. 
 
-I needed to use character arrays for the different types of character families, but instead of writing my own arrays, I found how javascript uses the fromCharCode() method to retrieve characters from the ascii table. 
+I needed to use character arrays for the different types of character families. Instead of writing my own arrays, I found how javascript uses the fromCharCode() method to retrieve characters from the ascii table. Compared to building arrays of characters, I think this is the cleaner and more professional option.
 
-The final function of the project is a shuffling array designed to give the password a sense of randomness. This ended up being the most difficult piece of the project because it needed to copy elements and swap positions of two array. I was determined to get this piece in because it pushed my limits of my logical javascript capabilities.
+The final function of the project is a shuffling array designed to give the password a sense of randomness. This ended up being the most difficult piece of the project because it needed to copy elements and swap positions of two arrays. I was determined to get this piece in because it pushed the limits of my logical javascript capabilities.
 
 # Credits
 Thank you for your help along the way!
@@ -44,6 +44,7 @@ Thank you for your help along the way!
 * [Dillon Hoban](https://github.com/dillonhoban)
 * [Emily Thomas](https://github.com/ethomas22)
 * [Ravi Ganesan](https://github.com/ravifindravicom)
+* [Sean Chang](https://github.com/highwolfx)
 
 # License
 This repository is released under the [MIT License](https://opensource.org/licenses/MIT)
--- assets/script.js
+++ assets/script.js
@@ -16,138 +16,75 @@ function writePassword() {
 // Event listener for Generate Password button
 generateBtn.addEventListener("click", writePassword);
 
-//Function that generates a password
+// Function that generates a password
 function generatePassword() {
-	//Prompts user for desired password length
+	// Prompts user for desired password length
 	while (true) {
 		var pwdLength = prompt("How many characters in length is your password?");
-		//Password length limiters
+		// Password length limiters
 		if ((pwdLength >= 8) && (pwdLength <= 128)) {
 			break;
 		} else (alert("Your password must be at least 8 characters long and no more than 128 characters in length."));
 	}
 
-	//User chooses desired character types
+	// User chooses desired character types
 	while (true) {
 		var pwdLower = confirm("Add lowercase characters?");
 		var pwdUpper = confirm("Add uppercase characters?");
 		var pwdNumeric = confirm("Add numerical characters?");
 		var pwdSpecial = confirm("Add special characters?");
-		//Password must contain at least one character type
+		// Password must contain at least one character type
 		if ((pwdLower === true) || (pwdUpper === true) || (pwdNumeric === true) || (pwdSpecial === true)) {
 			break;
 		} else (alert("Your password must contain at least one type of character."));
 	}
 
-	// Generates random character for each character type
-	var lowerBank = String.fromCharCode(randomNum(97, 122));
-	var upperBank = String.fromCharCode(randomNum(65, 90));
-	var numericBank = String.fromCharCode(randomNum(48, 57));
-	var specialBank = String.fromCharCode(randomNum(33, 47));
-
 	// Generates array of characters for password
-	var charArray = [];
+	var generatedChars = [];
 	var counter = 0;
 	while (counter < pwdLength) {
 		if (pwdLower) {
-			charArray.push(String.fromCharCode(randomNum(97, 122)));
+			generatedChars.push(String.fromCharCode(randomNum(97, 122)));
 			counter++;
 		}
 		if (pwdUpper) {
-			charArray.push(String.fromCharCode(randomNum(65, 90)));
+			generatedChars.push(String.fromCharCode(randomNum(65, 90)));
 			counter++;
 		}
 		if (pwdNumeric) {
-			charArray.push(String.fromCharCode(randomNum(48, 57)));
+			generatedChars.push(String.fromCharCode(randomNum(48, 57)));
 			counter++;
 		}
 		if (pwdSpecial) {
-			charArray.push(String.fromCharCode(randomNum(33, 47)));
+			generatedChars.push(String.fromCharCode(randomNum(33, 47)));
 			counter++;
 		}
 	}
 
-//test start
-//Array for on/off char type
-	// var charTypeArray = [pwdLower, pwdUpper, pwdNumeric, pwdSpecial];
-	// counter2 = 0;
-	// while (counter2 < pwdLength) {
-	// 	for (i = 0; i < pwdLength; i++) {
-	// 		function generateChar(charType, asciiMin, asciiMax) {
-	// 			charArray.push(String.fromCharCode(randomNum(asciiMin, asciiMax)));
-				
-	// 		}
-	// 	}
-	// }
-
-//test end
-
-
-	//Generates string of chars = password length without omitting any desired char types
-	function stringMaker() {
+	// Trims password to length without omitting any desired char types
+	function stringMaker(length, array) {
 		var str = '';
-		for (i = 0; i < pwdLength; i++) {
-			str += charArray[((i) % charArray.length)];
+		for (i = 0; i < length; i++) {
+			str += array[((i) % array.length)];
 		}
 		return str;
 	}
-	
-	console.log("chararray " + charArray);
-	console.log("tostring " + charArray.toString());
-	console.log("stringMaker " + stringMaker());
 
+	// Define unshuffled string
+	var trimmedChars = stringMaker(pwdLength, generatedChars);
 
-
-
-	//Converting string to array
-	var bigArray = (stringMaker()).split('');
-	//Blank string to return at end of function
-	var masterStr = '';
-	//String shuffler
-	for (i = 0; i < (bigArray.length - 1); i++) {
-		var j = randomNum(0, (bigArray.length));	//Random index j
-		var temp = bigArray[i];						//Stored element value from array
-		bigArray[i] = bigArray[j];					//Swap element index i to index j
-		bigArray[j] = temp; 						//Copy element value to index j
+	// String shuffler
+	function arrayShuffler(str) {;
+		array = str.split('');
+		for (i = 0; i < (array.length - 1); i++) {
+			var j = randomNum(0, (array.length));	//Random index j
+			var temp = array[i];					//Stored element value from array
+			array[i] = array[j];					//Swap element index i to index j
+			array[j] = temp; 						//Copy element value to index j
+		}
+		var shuffled = array.join('');
+		return shuffled;
 	}
-	masterStr = bigArray.join('');
-	return masterStr;
-
+	// Shuffled string
+	return arrayShuffler(trimmedChars);
 };
-
-
-	// //Generates fraction so that password length is divided evenly by char types
-	// function fractioner() {
-	// 	var num = 0;
-	// 	var charTypeArray = [pwdLower, pwdUpper, pwdNumeric, pwdSpecial];
-	// 	for (i = 0; i < charTypeArray.length; i++) {
-	// 		if (charTypeArray[i] === true) {
-	// 			num++;
-	// 		}
-	// 	}
-	// 	return num;
-	// }
-
-	// //Generates string of characters
-	// function stringMaker(length, fraction, asciiMin, asciiMax) {
-	// 	var str = '';
-	// 	for (i = 0; i < (length / fraction); i++) {
-	// 		str += (String.fromCharCode(randomNum(asciiMin, asciiMax)));
-	// 	}
-	// 	return str;
-	// }
-
-	// bigStr = '';
-	// //Parameters for specific types of characters
-	// if (pwdLower) {
-	// 	bigStr += stringMaker(pwdLength, (fractioner()), 97, 122);
-	// }
-	// if (pwdUpper) {
-	// 	bigStr += stringMaker(pwdLength, (fractioner()), 65, 90);
-	// }
-	// if (pwdNumeric) {
-	// 	bigStr += stringMaker(pwdLength, (fractioner()), 48, 57);
-	// }
-	// if (pwdSpecial) {
-	// 	bigStr += stringMaker(pwdLength, (fractioner()), 33, 47);
-	// }
